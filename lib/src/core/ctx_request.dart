b563b2e38a63a5f9c01daff17c963a1140a51a3b
Gustavo Guzman <guzmanlo.2001@gmail.com>
2025-11-10 19:16:26 +0100

    fix: query parameters

--- lib/src/core/ctx_request.dart
+++ lib/src/core/ctx_request.dart
@@ -52,8 +52,38 @@ class CtxRequest {
   /// Path component of the request URI.
   String get path => _request.uri.path;
 
-  /// All query parameters with all values preserved.
-  Map<String, List<String>> get queryParameters => Map.unmodifiable(_request.uri.queryParametersAll);
+  /// The URI query split into a map according to the rules
+  /// specified for FORM post in the [HTML 4.01 specification section
+  /// 17.13.4](https://www.w3.org/TR/REC-html40/interact/forms.html#h-17.13.4
+  /// "HTML 4.01 section 17.13.4").
+  ///
+  /// Each key and value in the resulting map has been decoded.
+  /// If there is no query, the empty map is returned.
+  ///
+  /// Keys in the query string that have no value are mapped to the
+  /// empty string.
+  /// If a key occurs more than once in the query string, it is mapped to
+  /// an arbitrary choice of possible value.
+  /// The [queryParametersAll] getter can provide a map
+  /// that maps keys to all of their values.
+  ///
+  /// The map is unmodifiable.
+  Map<String, String> get queryParameters => _request.uri.queryParameters;
+
+  /// Returns the URI query split into a map according to the rules
+  /// specified for FORM post in the [HTML 4.01 specification section
+  /// 17.13.4](https://www.w3.org/TR/REC-html40/interact/forms.html#h-17.13.4
+  /// "HTML 4.01 section 17.13.4").
+  ///
+  /// Each key and value in the resulting map has been decoded. If there is no
+  /// query, the map is empty.
+  ///
+  /// Keys are mapped to lists of their values. If a key occurs only once,
+  /// its value is a singleton list. If a key occurs with no value, the
+  /// empty string is used as the value for that occurrence.
+  ///
+  /// The map and the lists it contains are unmodifiable.
+  Map<String, List<String>> get queryParametersAll => _request.uri.queryParametersAll;
 
   /// Whether this request can currently be hijacked.
   bool get canHijack => _canHijack && !_hijacked;
